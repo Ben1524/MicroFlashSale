7ada17f022a6c57628d938d871b33d53a2cd9d78
ZJQ <wy17674094293@163.com>
2025-05-11 18:47:12 +0800

    更新前端路由

--- vue_fronted/src/router/index.ts
+++ vue_fronted/src/router/index.ts
@@ -1,19 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
-const routes: Array<RouteRecordRaw> = [
+const router = createRouter({
+    history:createWebHistory(),
+    routes:[
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'GoodsList',
+    component: () => import('../views/GoodsListView.vue')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/detail/:id',
+    name: 'GoodsDetail',
+    component: () => import(/* webpackChunkName: "about" */ '../views/GoodsDetailView.vue')
   },
   {
     path:'/login',
@@ -25,11 +23,36 @@ const routes: Array<RouteRecordRaw> = [
     name:'register',
     component: () => import('../views/RegisterView.vue')
   }
-]
+]})
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+// 路由守卫
+router.beforeEach((to,from,next) => {
+  // to 满足条件的url放过
+  if (to.path == "/" || to.path == "/login" || to.path == "/register" || to.path == "/admin"){
+    next()  // 直接放过
+  }else {
+    const reg = /\/main\/[a-zA-z]+[_-]?/
+    // 做token校验
+    if (reg.test(to.path)) {
+      // 校验管理员，不受用户影响
+      const admin_token = localStorage.getItem("admin_token")
+      // alert("到main的url")
+      if (admin_token == "" || admin_token == null || admin_token == '') {
+        next("/admin")
+      }else {
+        next()
+      }
+    }else {
+      // 校验用户的，不受管理员影响
+      const front_token = localStorage.getItem("front_token")
+      // front的需要权限的url
+      if (front_token == "" || front_token == null || front_token == '') {
+        next("/login")
+      }else {
+        next()
+      }
+    }
+  }
 })
 
 export default router
